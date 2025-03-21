6198e8385c60e726b998da147cdf2321a1785b17
ahmed.delta <ahmed.kishki@uni-rostock.de>
2025-03-21 17:56:29 +0100

    ö

--- qkd_main_CIFAR100.py
+++ qkd_main_CIFAR100.py
@@ -126,7 +126,7 @@ def main():
     # # ------------------------------
     retrials = 2
     kd_loss_labels = ['KL','CS']
-    alpha_st_pairs = [(0.5,0.5),(0.1,1.0)]
+    alpha_st_pairs = [(0.5,0.5),(1.0,0.1)]
     temperatures = [6.0]
     student_lr = 1e-4
     teacher_lr = 1e-6
